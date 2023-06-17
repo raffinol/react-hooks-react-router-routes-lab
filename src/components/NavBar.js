e6855ca607265981bcd5ec7ffe0884c30ed0413e
Rafael Finol <rafaelfinol88@gmail.com>
2023-06-17 18:48:29 -0400

    Home and NavBar ready

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,32 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const linkStyles = {
+  display: 'inline-block',
+  width: '50px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  textDecoration: 'none',
+  color: 'Blue',
+};
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink to="/" exact style={linkStyles} activeStyle={{ color: 'grey' }}>
+        Home
+      </NavLink>
+      <NavLink to="/movies" exact style={linkStyles} activeStyle={{ color: 'grey' }}>
+        Movies
+      </NavLink>
+      <NavLink to="/directors" exact style={linkStyles} activeStyle={{ color: 'grey' }}>
+        Directors
+      </NavLink>
+      <NavLink to="/actors" exact style={linkStyles} activeStyle={{ color: 'grey' }}>
+        Actors
+      </NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
